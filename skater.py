eb5cdf9763240d45aa46954a9c40f6dd5e84229d
Mike Slegeir <tehpola@gmail.com>
2023-03-21 16:25:29 -0500

    Handle the player going off the screen by resetting.
    Also, I removed the temporary grinding logic because it's too wacky as-is.

--- skater.py
+++ skater.py
@@ -15,16 +15,28 @@ class Skater(SpriteSheet):
         vy += self.gravity * dt
         self.velocity = vx, vy
 
+        # Move according to the velocity
         self.rect.move_ip(*self.velocity)
 
+        # Detect the character moving off the screen
+        ew, eh = state.env.rect.size
+        if self.rect.x > ew or self.rect.y > eh or self.rect.x < -self.rect.width:
+            self.rect.topleft = (0, 0)
+            self.velocity = min(vx, 10), 0
+            self.animate('float')
+            # TODO: Move to the next scene
+
+        # Environmental collision detection
         collision = pygame.sprite.collide_mask(state.env, self)
         if collision:
+            # TODO: I should do something better to resolve penetration...
             if state.env.is_dangerous(collision):
                 self.animate('falling')
             elif state.env.is_grindable(collision):
-                # TODO: Only collide while coming down...
-                self.land()
-                self.animate('manual')
+                # TODO: Just flag this?
+                pass
+                #self.land()
+                #self.animate('manual')
             elif state.env.is_rideable(collision):
                 self.land()
                 self.animate('riding')
