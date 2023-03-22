f06a7e75255f5bec3d75940b63f910979c7282a1
Mike Slegeir <tehpola@gmail.com>
2023-03-22 17:17:14 -0500

    Working to depenetrate collisions...
    This has been giving me a lot of grief and it's _close_, but still not there!
    Ultimately doing per-pixel collision leaves a lot of guessing and doesn't really offer any guidance on how to back out apart from an iterative approach...

--- skater.py
+++ skater.py
@@ -6,62 +6,94 @@ class Skater(SpriteSheet):
     def __init__(self, info, state):
         super().__init__(info)
         self.state = state
-        self.velocity = (0, 0)
-        self.gravity = 0.025
+        self.velocity = pygame.math.Vector2()
+        self.gravity = 0.0025
         self.is_grounded = False
 
+        self.debug_overlay = pygame.sprite.Sprite()
+        self.debug_overlay.image = pygame.surface.Surface(self.image.get_size()).convert_alpha()
+        pygame.draw.circle(self.debug_overlay.image, (255, 0, 0), (4, 4), 4)
+        self.debug_overlay.rect = self.debug_overlay.image.get_rect()
+
     def update(self, dt, *args, **kwargs):
+        ox, oy = self.rect.topleft
+        self.debug_overlay.image.fill((0, 0, 0, 0))
+
         # Apply gravity
-        vx, vy = self.velocity
-        vy += self.gravity * dt
-        self.velocity = vx, vy
+        if not self.is_grounded:
+            self.velocity.y += self.gravity * dt
+
+        center = pygame.math.Vector2(self.rect.size)/2
+        pygame.draw.line(self.debug_overlay.image, (0, 0, 255), center, center + self.velocity * dt)
 
         # Move according to the velocity
-        self.rect.move_ip(*self.velocity)
+        self.rect.move_ip(*(self.velocity * dt))
+        self.debug_overlay.rect = self.rect
 
         # Detect the character moving off the screen
         ew, eh = self.state.env.rect.size
         if self.rect.x > ew or self.rect.y > eh or self.rect.x < -self.rect.width:
             self.rect.topleft = (0, 0)
-            self.velocity = min(vx, 10), 0
+            self.velocity.x = min(max(self.velocity.x, -10), 10)
+            self.velocity.y = 0
             self.animate('float')
             # TODO: Move to the next scene
+            return
 
         # Environmental collision detection
         collision = pygame.sprite.collide_mask(self.state.env, self)
         if collision:
-            # TODO: I should do something better to resolve penetration...
-            if self.state.env.is_dangerous(collision):
+            icol = collision
+            collision = pygame.math.Vector2(collision)
+            pygame.draw.circle(self.debug_overlay.image, (255, 0, 0), collision - self.rect.topleft, 4)
+            self.state.world.add(self.debug_overlay)
+
+            if self.state.env.is_dangerous(icol):
                 self.animate('falling')
-            elif self.state.env.is_grindable(collision):
+            elif self.state.env.is_grindable(icol):
                 # TODO: Just flag this?
                 pass
                 #self.land()
                 #self.animate('manual')
-            elif self.state.env.is_rideable(collision):
-                self.land()
+            elif self.state.env.is_rideable(icol):
+                self.land(collision, dt)
                 self.animate('riding')
             else:
                 pass # wat do? Erorr!
+        else:
+            self.state.world.remove(self.debug_overlay)
+
+            # Check whether we're rolling off the ground...
+            gx, gy = self.rect.bottomleft
+            gy += 2
+            gx += (1 if self.velocity.x >= 0 else 2) * (0.3 * self.rect.width)
+            if not self.state.env.is_rideable((int(gx), int(gy))):
+                self.is_grounded = False
 
         SpriteSheet.update(self, dt)
 
-    def land(self):
-        vx, _ = self.velocity
-        self.rect.move_ip(0, -1)
-        self.velocity = vx, 0
+    def land(self, collision, dt):
+        # Resolve penetration
+        if self.velocity:
+            to_col = collision - self.rect.center
+            corner = pygame.math.Vector2()
+            corner.x = self.rect.left if to_col.x < 0 else self.rect.right
+            corner.y = self.rect.top if to_col.y < 0 else self.rect.bottom
+            back = (collision - corner).project(-self.velocity * dt)
+            self.rect.move_ip(*back)
+
+        self.velocity.y = 0
         self.is_grounded = True
 
     def handle(self, event):
         if event.type == pygame.KEYDOWN:
             name = pygame.key.name(event.key)
-            vx, vy = self.velocity
             if name == 'right':
-                self.velocity = vx + 2, vy
+                self.velocity.x += 0.1
             elif name == 'left':
-                self.velocity = vx - 2, vy
+                self.velocity.x -= 0.1
             elif name == 'space' and self.is_grounded:
-                self.velocity = vx, -15
+                self.velocity.y = -1
                 self.is_grounded = False
                 self.animate('ollie')
 
